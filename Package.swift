cc304012a5fd6cfe32ce79fe5630248078bfbaa3
Jaim Zuber <jaim@sharpfivesoftware.com>
2020-05-02 08:44:39 -0500

    Add library

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "Oliva",
+    products: [
+        .library(name: "Oliva", targets: ["ViewModels"]),
+    ],
     dependencies: [
     .package(url: "https://github.com/JohnSundell/Plot.git", from: "0.7.0"),
     .package(url: "https://github.com/johnsundell/publish.git", from: "0.1.0")
