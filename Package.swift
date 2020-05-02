4143c21a19c59d5183d0e24ff5ce5ea0e8290f86
Jaim Zuber <jaim@sharpfivesoftware.com>
2020-05-02 09:41:57 -0500

    Create OlivaDomain

--- Package.swift
+++ Package.swift
@@ -5,12 +5,12 @@ import PackageDescription
 let package = Package(
     name: "Oliva",
     products: [
-        .library(name: "Oliva", targets: ["ViewModels"]),
+        .library(name: "Oliva", targets: ["OlivaDomain"]),
         .executable(name: "LeagueResultsSite", targets: ["LeagueResultsSite"])
     ],
     dependencies: [
-    .package(url: "https://github.com/JohnSundell/Plot.git", from: "0.7.0"),
-    .package(url: "https://github.com/johnsundell/publish.git", from: "0.1.0")
+        .package(url: "https://github.com/JohnSundell/Plot.git", from: "0.7.0"),
+        .package(url: "https://github.com/johnsundell/publish.git", from: "0.1.0")
     ],
     targets: [
         .target(
@@ -18,7 +18,7 @@ let package = Package(
             dependencies: ["Plot", "Publish"]
         ),
         .target(
-            name: "ViewModels",
+            name: "OlivaDomain",
             dependencies: []
         )
     ]
